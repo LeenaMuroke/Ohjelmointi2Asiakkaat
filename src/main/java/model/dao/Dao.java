fb65757bd75239a4d2449c5a02f1881f92ed77f5
Leena Muroke <muroke.leena@gmail.com>
2022-11-20 19:25:26 +0200

    Eka ajo

---
+++ src/main/java/model/dao/Dao.java
@@ -0,0 +1,129 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.sql.PreparedStatement;
+import java.sql.DriverManager;
+import model.Asiakas;
+
+
+public class Dao {
+	
+	private Connection con = null;
+	private ResultSet rs = null;
+	private PreparedStatement stmtPrep = null;
+	private String sql;
+	private String db = "Myynti.sqlite";	
+	
+	public Connection yhdista() {
+		
+		Connection con = null;
+		String path = System.getProperty("catalina.base");
+		path = path.substring(0, path.indexOf(".metadata")).replace("\\", "/");
+		String url = "jdbc:sqlite:" + path + db;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection(url);
+			System.out.println("Yhteys avattu.");
+		} catch (Exception e) {
+			System.out.println("Yhteyden avaus epäonnistui.");
+			e.printStackTrace();
+		}
+		return con;
+	
+	}
+	
+	private void sulje() {
+		
+		if (stmtPrep != null) {
+			try {
+				stmtPrep.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (con != null) {
+			try {
+				con.close();
+				System.out.println("Yhteys suljettu.");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public ArrayList<Asiakas> getAllItems() {
+		
+		ArrayList<Asiakas> asiakkaat = new ArrayList<Asiakas>();
+		sql = "SELECT * FROM asiakkaat ORDER BY asiakas_id DESC";
+		try {
+			con = yhdista();
+			if (con != null) {
+				stmtPrep = con.prepareStatement(sql);
+				rs = stmtPrep.executeQuery();
+				if (rs != null) {
+					while (rs.next()) {
+						Asiakas asiakas = new Asiakas();
+						asiakas.setAsiakas_id(rs.getInt(1));
+						asiakas.setEtunimi(rs.getString(2));
+						asiakas.setSukunimi(rs.getString(3));
+						asiakas.setPuhelin(rs.getString(4));
+						asiakas.setSposti(rs.getString(5));
+						asiakkaat.add(asiakas);
+					}
+				}
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+		} finally {
+			sulje();
+		}
+		return asiakkaat;
+		
+	}
+	
+public ArrayList<Asiakas> getAllItems(String searchStr) {
+		
+		ArrayList<Asiakas> asiakkaat = new ArrayList<Asiakas>();
+		sql = "SELECT * FROM asiakkaat WHERE asiakas_id LIKE ? or etunimi LIKE ? or sukunimi LIKE ? or puhelin LIKE ? or sposti LIKE ? ORDER BY asiakas_id DESC";
+		try {
+			con = yhdista();
+			if (con != null) {
+				stmtPrep = con.prepareStatement(sql);
+				stmtPrep.setString(1, "%" + searchStr + "%");
+				stmtPrep.setString(2, "%" + searchStr + "%");
+				stmtPrep.setString(3, "%" + searchStr + "%");
+				stmtPrep.setString(4, "%" + searchStr + "%");
+				stmtPrep.setString(5, "%" + searchStr + "%");
+				rs = stmtPrep.executeQuery();
+				if (rs != null) {
+					while (rs.next()) {
+						Asiakas asiakas = new Asiakas();
+						asiakas.setAsiakas_id(rs.getInt(1));
+						asiakas.setEtunimi(rs.getString(2));
+						asiakas.setSukunimi(rs.getString(3));
+						asiakas.setPuhelin(rs.getString(4));
+						asiakas.setSposti(rs.getString(5));
+						asiakkaat.add(asiakas);
+					}
+				}
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+		} finally {
+			sulje();
+		}
+		return asiakkaat;
+		
+	}
+
+}
---
+++ src/main/webapp/ListaaAsiakkaat.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<script src="scripts/main.js"></script>
+<link rel="stylesheet" type="text/css" href="styles.css">
+<title>Asiakkaiden listaus</title>
+</head>
+<body>
+	<h1>Asiakkaiden listaus</h1>
+	<p>Tällä sivulla voit hakea asiakkaita tietokannasta hakuasanan avulla</p>
+	<table id="listaus">
+		<thead>
+			<tr>
+				<th>Hakusana: </th>
+				<th colspan="3"><input type="text" id="hakusana"></th>
+				<th><input type="button" value="hae" id="hakunappi" onclick="haeAsiakkaat()"></th>
+			</tr>
+			<tr>
+				<th>Asiakas ID</th>
+				<th>Etunimi</th>
+				<th>Sukunimi</th>
+				<th>Puhlinnumero</th>
+				<th>Sähköposti</th>
+			</tr>
+		</thead>
+		<tbody id="tbody">
+		</tbody>
+	</table>
+	<span id="ilmo"></span>
+	<script>
+		haeAsiakkaat();
+	</script>
+</body>
+</html>
